f1efe7f9687edaa783bcee6607b91667459cc74f
Peng Wang <Sansiwang9527@gmail.com>
2025-10-14 22:29:41 -0400

    style: 鏍煎紡鍖?vite.config.js 浠ｇ爜

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   // 在开发环境中使用根路径，在生产环境中使用 GitHub Pages 子路径
-  const base = command === 'serve' ? '/' : '/cst8504-aat-midterm-practice-project/';
-  
+  const base =
+    command === "serve" ? "/" : "/cst8504-aat-midterm-practice-project/";
+
   return {
     plugins: [react()],
     base: base,
